6713e3aa95477113025b940d5c613fb84d4ffc7f
NelipeFunes <miotarestaurante2003@gmail.com>
2021-12-09 14:34:59 -0300

    End

--- src/algumacoiusa.js
+++ src/algumacoiusa.js
@@ -1,14 +1,13 @@
-let myArray = [];
-
-function techList(technologies,name) {
-    for(let i = 0;i<technologies.length; i+=1){
-       myArray.push({
-        tech:technologies[i],
-        name:name
-       }) 
+function generatePhoneNumber(numbers) {
+    if(numbers.length!==11){
+        return ("Array com tamanho incorreto")
     }
-    return myArray 
-  }
-  console.log(techList(['java','script','otavio'],'felipe'))
+    else{
+    let phoneNumber = "("+numbers[0]+numbers[1]+")"+" "+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+"-"+numbers[7]+numbers[8]+numbers[9]+numbers[10]
+    return phoneNumber
+    }
+}
+
+console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]))
   
   
--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,8 @@ function techList(technologies,name) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  
+function generatePhoneNumber(numbers) {
+  let phoneNumber = "("+numbers[0]+numbers[1]+")"+" "+numbers[2]+numbers[3]+numbers[4]+numbers[5]+"-"+numbers[6]+numbers[7]+numbers[8]+numbers[9]
 }
 
 // Desafio 12
