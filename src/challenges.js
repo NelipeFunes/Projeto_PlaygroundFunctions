c9d57564f6354501b7aabef07a7e072af03a2672
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-04-12 21:45:51 -0300

    Finaliza Projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,15 @@
 // Desafio 1
-function compareTrue(p1,p2) {
-  if(p1=== true && p2===true){
-  return true;
-}
-else{
-  return false;
-}
+function compareTrue(p1, p2) {
+  if (p1 === true && p2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base, height) {
   let area = (base * height) / 2;
   return area;
-  
 }
 
 // Desafio 3
@@ -22,13 +20,13 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(myArray) {
-   let first = myArray[0];
-   let last = myArray[myArray.length - 1];
-   return (last + ', ' + first);
+  let first = myArray[0];
+  let last = myArray[myArray.length - 1];
+  return last + ', ' + first;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   let pointsVictory = 3 * wins;
   let pointsTies = 1 * ties;
   let totalPoints = pointsVictory + pointsTies;
@@ -39,14 +37,14 @@ function footballPoints(wins,ties) {
 function highestCount(numbers) {
   let tempNumber = numbers[0];
   let numberRepetition = 0;
-  for(let i = 1;i<numbers.length;i+=1){
-    if(tempNumber<numbers[i]){
+  for (let i = 1; i < numbers.length; i += 1) {
+    if (tempNumber < numbers[i]) {
       tempNumber = numbers[i];
     }
   }
-  for(i=0 ;i<numbers.length; i+=1){
-    if(numbers[i]===tempNumber){
-      numberRepetition+=1;
+  for (i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] === tempNumber) {
+      numberRepetition += 1;
     }
   }
 
@@ -54,104 +52,74 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
-  let dist1 = Math.abs(mouse-cat1);
-  let dist2 = Math.abs(mouse-cat2);
-  let phrase = "os gatos trombam e o rato foge"
-  if(dist1<dist2){
-    return ('cat1');
-  }
-  else if(dist2<dist1){
-  return ('cat2');
-  }
-  else if (dist2===dist1)
-  return phrase;
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+  let phrase = 'os gatos trombam e o rato foge';
+  if (dist1 < dist2) {
+    return 'cat1';
+  } else if (dist2 < dist1) {
+    return 'cat2';
+  } else if (dist2 === dist1) return phrase;
 }
 
 // Desafio 8
 function fizzBuzz(numbers) {
   let phrase = [];
-  for(let i = 0; i<numbers.length; i+=1){
-    if(numbers[i]%3 === 0 && numbers[i]%5 === 0){
-      phrase.push("fizzBuzz")
-    }
-    else if(numbers[i]%3 === 0){
-      phrase.push("fizz")
-    }
-    else if(numbers[i]%5 === 0){
-      phrase.push("buzz")
-    }
-    else{
-      phrase.push("bug!")
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+      phrase.push('fizzBuzz');
+    } else if (numbers[i] % 3 === 0) {
+      phrase.push('fizz');
+    } else if (numbers[i] % 5 === 0) {
+      phrase.push('buzz');
+    } else {
+      phrase.push('bug!');
     }
   }
-return phrase
-
+  return phrase;
 }
 
 // Desafio 9
 function encode(string) {
-  
-  let splitedString = string.split('')
-   
-  for(let i = 0;i<splitedString.length;i+=1){
-    if(splitedString[i] === "a"){
-      splitedString[i] = "1"
-    }
+  let splitedString = string.split('');
 
-    else if(splitedString[i] === "e"){
-      splitedString[i] = "2"
+  for (let i = 0; i < splitedString.length; i += 1) {
+    if (splitedString[i] === 'a') {
+      splitedString[i] = '1';
+    } else if (splitedString[i] === 'e') {
+      splitedString[i] = '2';
+    } else if (splitedString[i] === 'i') {
+      splitedString[i] = '3';
+    } else if (splitedString[i] === 'o') {
+      splitedString[i] = '4';
+    } else if (splitedString[i] === 'u') {
+      splitedString[i] = '5';
     }
-    
-    else if(splitedString[i] === "i"){
-      splitedString[i] = "3"
-    }
-    
-    else if(splitedString[i] === "o"){
-      splitedString[i] = "4"
-    }
-    
-    else if(splitedString[i] === "u"){
-      splitedString[i] = "5"
-    }
-   
   }
-   splitedString = splitedString.join("")
-   return splitedString
+  splitedString = splitedString.join('');
+  return splitedString;
 }
 
 function decode(string) {
- 
-  let splitedString = string.split('')
-   
-  for(let i = 0;i<splitedString.length;i+=1){
-    if(splitedString[i] === "1"){
-      splitedString[i] = "a"
-    }
+  let splitedString = string.split('');
 
-    else if(splitedString[i] === "2"){
-      splitedString[i] = "e"
+  for (let i = 0; i < splitedString.length; i += 1) {
+    if (splitedString[i] === '1') {
+      splitedString[i] = 'a';
+    } else if (splitedString[i] === '2') {
+      splitedString[i] = 'e';
+    } else if (splitedString[i] === '3') {
+      splitedString[i] = 'i';
+    } else if (splitedString[i] === '4') {
+      splitedString[i] = 'o';
+    } else if (splitedString[i] === '5') {
+      splitedString[i] = 'u';
     }
-    
-    else if(splitedString[i] === "3"){
-      splitedString[i] = "i"
-    }
-    
-    else if(splitedString[i] === "4"){
-      splitedString[i] = "o"
-    }
-    
-    else if(splitedString[i] === "5"){
-      splitedString[i] = "u"
-    }
-   
   }
-   splitedString = splitedString.join("")
-   return splitedString
-
+  splitedString = splitedString.join('');
+  return splitedString;
 }
-
-
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -43,14 +43,46 @@ function generatePhoneNumber(numbers) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a,b,c) {
+  let result = false;
+  if(a < b + c && b < a + c && c < b + a) {
+    result = true;
+  }
+  return result;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(array) {
+  let sum = 0;
+  const splited = array.split(' ');
+  splited.forEach((element) => {
+    if(element === '1') {
+      sum += 1;
+    } else if (element === '2') {
+      sum += 2
+    } else if (element === '3') {
+      sum += 3
+    } else if (element === '4') {
+      sum += 4
+    } else if (element === '5') {
+      sum += 5
+    } else if (element === '6') {
+      sum += 6
+    } else if (element === '7') {
+      sum += 7
+    } else if (element === '8') {
+      sum += 8
+    } else if (element === '9') {
+      sum += 9
+    }
+  });
+  if(sum === 1) {
+    return (`${sum} copo de água`)
+  }
+  return (`${sum} copos de água`)
 }
+
+console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
 
 module.exports = {
   generatePhoneNumber,
